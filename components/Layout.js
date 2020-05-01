6923aaf32de30290a73ce07f8b90d2aef4716a3c
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-05-01 15:46:06 -0500

    se corrigió loader para verstión next 9+

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,9 @@ import Head from 'next/head';
 import NProgress from 'nprogress';
 import { Router } from 'next/router';
 
-Router.onRouteChangeStart = (url) => {
-  NProgress.start();
-};
-Router.onRouteChangeComplete = () => NProgress.done();
-Router.onRouteChangeError = () => NProgress.done();
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default class Layout extends React.Component {
   render() {
